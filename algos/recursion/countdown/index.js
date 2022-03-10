4208b1577ad87797ccb2ed4dc81762277d3254a9
mushfikaraisa <76073843+mushfikaraisa@users.noreply.github.com>
2022-03-10 12:27:48 -0600

    Update index.js

--- algos/recursion/countdown/index.js
+++ algos/recursion/countdown/index.js
@@ -1,10 +1,11 @@
 function countdown(n){
-   
-    if(n == 1){
-    return 1;
+    let countdownString = ""
+    if(n <= 0){
+    return countdownString + "Liftoff!";
+  } else {
+    countdownString += `${n}, `
   }
-    
-    return countdown(n - 1)
+     return countdownString += countdown(n - 1)
   }
 console.log(countdown(5));
 module.exports = countdown
