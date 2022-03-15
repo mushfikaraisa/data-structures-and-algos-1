9b09b2458bc9bf4d608d40739f33f10a7e67ffd7
mushfikaraisa <76073843+mushfikaraisa@users.noreply.github.com>
2022-03-15 14:57:38 -0500

    stacks & Queues

--- data-structures/stacks/index.js
+++ data-structures/stacks/index.js
@@ -1,8 +1,24 @@
 class Stack{
 	// Your code below!
 	constructor(){
-	
+		this.items = [];
+		this.size= 0;
 	}
+	push(element)
+{
+	let numberOfItems = this.items.length;
+	this.items.push(element);
+	if (this.items.length-numberOfItems===1) {
+		this.size+=1 }
+}
+pop()
+{
+    // return top most element in the stack
+    // and removes it from the stack
+    // Underflow if stack is empty
+    this.size-=1;
+    return this.items.pop();
+}
 }
 
 module.exports = Stack
