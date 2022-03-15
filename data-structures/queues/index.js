dc7ece56b02894fd02c2fa4d885c8a23ed8be689
mushfikaraisa <76073843+mushfikaraisa@users.noreply.github.com>
2022-03-15 15:00:40 -0500

    Update index.js
    modified

--- data-structures/queues/index.js
+++ data-structures/queues/index.js
@@ -1,8 +1,31 @@
 class Queue {
 	// Your code below!
 	constructor() {
-
+		this.items = [];
+		this.size= 0;
 	}
+
+	enqueue(element)
+{    
+	let numberOfItems = this.items.length;
+	
+	// adding element to the queue
+	this.items.push(element);
+	if (this.items.length-numberOfItems===1) {
+		this.size+=1 }
+}
+dequeue()
+
+{
+    // removing element from the queue
+    // returns underflow when called 
+	// on empty queue
+	
+		this.size-=1 
+		return this.items.shift();
+}
 }
 
 module.exports = Queue
+
+
