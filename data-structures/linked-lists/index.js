6b2a60496ed0d2cb8cce4af2de684c50bf6268c1
mushfikaraisa <76073843+mushfikaraisa@users.noreply.github.com>
2022-03-17 16:00:48 -0500

    Update index.js

--- data-structures/linked-lists/index.js
+++ data-structures/linked-lists/index.js
@@ -1,15 +1,58 @@
-class Node {
-  // Your code here!
-  constructor () {
+javascript
 
+class Node {
+  constructor (value) {
+    this.value = value
+    this.previous = null
+    this.next = null
   }
 }
 
 class LinkedList {
-  // Your code here!
   constructor () {
-
+    this.head = null
+    this.tail = null
   }
+
+  addToHead (node) {
+
+    const formerHead = this.head
+    this.head = node;
+
+    if (formerHead) {
+      formerHead.previous = node
+      node.next = formerHead
+    } else {
+      this.tail = node
+    }
+  }
+
+  addToTail (node) {
+
+    const formerTail = this.tail
+    this.tail = node
+    if (formerTail) {
+      formerTail.next = node
+      node.previous = formerTail
+    } else {
+      this.head = node
+    }
+  }
+
+
+  removeTail () {
+    const removedTail = this.tail
+    if (!removedTail) return null
+    if (removedTail.previous) {
+      this.tail = removedTail.previous
+      this.tail.next = null
+    } else {
+      this.head = null
+      this.tail = null
+    }
+    return removedTail
+  }
+
 }
 
 module.exports = {
