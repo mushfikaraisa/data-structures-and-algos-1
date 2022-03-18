1e93832c7fb5d9e6c38916c6858c3a2c425e9cf4
mushfikaraisa <76073843+mushfikaraisa@users.noreply.github.com>
2022-03-18 11:37:36 -0500

    Update index.js

--- data-structures/bst/index.js
+++ data-structures/bst/index.js
@@ -1,8 +1,34 @@
 class BinarySearchTree {
-  // Your code here!
-  constructor () {
-
+  /**
+   *
+   *  5
+   * / \
+   *2   6
+   / \   \
+  4   3   7
+   *
+   *
+   */
+  constructor(value) {
+    this.value = value;
+    this.right = null;
+    this.left = null;
   }
-}
-
+  insert(value) {
+    let binaryTree = new BinarySearchTree(value);
+    if (value < this.value) {
+      if (this.left) {
+        this.left.insert(value);
+      } else {
+        this.left = binaryTree;
+      }
+    } else {
+      if (this.right) {
+        this.right.insert(value);
+      } else {
+        this.right = binaryTree;
+      }
+    }
+  }
+  
 module.exports = BinarySearchTree
